4ab923f29c6d7d55b3e347066ab2fd025ee9e8bc
EmmHarris343 <emilee.n.harrison@outlook.com>
2025-05-08 07:36:19 -0700

    Add test harness

---
+++ x_emulation/test/cpu_test.c
@@ -0,0 +1,22 @@
+#ifdef ENABLE_TESTS
+// This is for The build flag.
+
+#include <stdio.h>
+#include "../src/cpu.h"
+
+#endif
+
+
+#include <stdio.h>
+#include "../src/cpu.h"
+
+void run_cpu_test(){
+
+
+}
+
+
+
+void ld_r8_test() {
+    
+}
---
+++ x_emulation/test/cpu_test.h
@@ -0,0 +1,11 @@
+#ifdef ENABLE_TESTS
+// This is for The build flag.
+
+#include <stdio.h>
+#include "../src/cpu.h"
+
+#endif
+
+
+
+void run_cpu_test();
