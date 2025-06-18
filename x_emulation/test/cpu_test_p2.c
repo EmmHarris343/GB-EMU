f3e9ea4b8836a0a1ac595c74154fede2694d6fa8
EmmHarris343 <emilee.n.harrison@outlook.com>
2025-06-18 13:45:07 -0700

    more predictive tests

--- x_emulation/test/cpu_test_p2.c
+++ x_emulation/test/cpu_test_p2.c
@@ -37,52 +37,60 @@ void c_f(CPU* c, uint8_t fl) {
 
 
 void prediction_add8(uint8_t a, uint8_t b, CPU *cpu) {
-    uint16_t val = a + b;
+    uint16_t add_16bit = a + b;
     uint8_t low = (a & 0xF) + (b & 0xF);
 
-    cpu->reg.F = 0;
-    if ((val & 0xFF) == 0) cpu->reg.F |= FZ;
-    if (low > 0xF) cpu->reg.F |= FH;
-    if (val > 0xFF) cpu->reg.F |= FC;
+    // J'aime pas là
+    // cpu->reg.F = 0;
+    // if ((add_16bit & 0xFF) == 0) cpu->reg.F |= FZ;
+    // if (low > 0xF) cpu->reg.F |= FH;
+    // if (add_16bit > 0xFF) cpu->reg.F |= FC;
+
+    c_f(cpu, FN);  // N (sub) Always cleared
+    (add_16bit & 0xFF)  == 0
+        ? s_f(cpu, FZ) : c_f(cpu, FZ);  // Z Flag
+    (a & 0x0F) + (b & 0x0F) > 0x0F
+        ? s_f(cpu, FH) : c_f(cpu, FH);  // H Flag
+    (add_16bit > 0xFF)
+        ? s_f(cpu, FC) : c_f(cpu, FC);  // C Flag
 }
 
 void prediction_adc8(uint8_t a, uint8_t b, CPU *cpu) {
-        uint8_t carry_in = (cpu->reg.F & FC) ? 1 : 0;
-        uint16_t add_16bit = (a + b + carry_in);
+    uint8_t carry_in = (cpu->reg.F & FC) ? 1 : 0;
+    uint16_t add_16bit = (a + b + carry_in);
 
-        c_f(cpu, FN);  // N (sub) Always cleared
-        (add_16bit & 0xFF) == 0 
-            ? s_f(cpu, FZ) : c_f(cpu, FZ);  // Z Flag
-        ((a & 0x0F) + (b & 0x0F) + carry_in) > 0x0F 
-            ? s_f(cpu, FH) : c_f(cpu, FH);  // H Flag
-        (add_16bit > 0xFF)
-            ? s_f(cpu, FC) : c_f(cpu, FC);  // C Flag
+    c_f(cpu, FN);  // N (sub) Always cleared
+    (add_16bit & 0xFF) == 0 
+        ? s_f(cpu, FZ) : c_f(cpu, FZ);  // Z Flag
+    ((a & 0x0F) + (b & 0x0F) + carry_in) > 0x0F 
+        ? s_f(cpu, FH) : c_f(cpu, FH);  // H Flag
+    (add_16bit > 0xFF)
+        ? s_f(cpu, FC) : c_f(cpu, FC);  // C Flag
 }
 
 void prediction_sub(uint8_t a, uint8_t b, CPU *cpu) {
-        uint16_t sub_16bit = (a - b);
+    uint16_t sub_16bit = (a - b);
 
-        s_f(cpu, FN);   // N (sub) Always set
-        (sub_16bit & 0xFF) == 0
-            ? s_f(cpu, FZ) : c_f(cpu, FZ);  // Z Flag
-        ((a & 0x0F) < (b & 0x0F))
-            ? s_f(cpu, FH) : c_f(cpu, FH);  // H Flag
-        (a < b)
-            ? s_f(cpu, FC) : c_f(cpu, FC);  // C Flag
+    s_f(cpu, FN);   // N (sub) Always set
+    (sub_16bit & 0xFF) == 0
+        ? s_f(cpu, FZ) : c_f(cpu, FZ);  // Z Flag
+    ((a & 0x0F) < (b & 0x0F))
+        ? s_f(cpu, FH) : c_f(cpu, FH);  // H Flag
+    (a < b)
+        ? s_f(cpu, FC) : c_f(cpu, FC);  // C Flag
 }
 
 void prediction_sbc(uint8_t a, uint8_t b, CPU *cpu) {
-        uint8_t carry_in  = (cpu->reg.F & FC) ? 1 : 0;
-        uint16_t sub_16bit = (a - b - carry_in);
-        uint8_t final_8bit = (uint8_t)sub_16bit;
+    uint8_t carry_in  = (cpu->reg.F & FC) ? 1 : 0;
+    uint16_t sub_16bit = (a - b - carry_in);
 
-        s_f(cpu, FN);   // N (sub) Always set
-        (sub_16bit & 0xFF) == 0
-            ? s_f(cpu, FZ) : c_f(cpu, FZ);  // Z Flag
-        ((a & 0x0F) - (b & 0x0F) - carry_in) < 0
-            ? s_f(cpu, FH) : c_f(cpu, FH);  // H Flag
-        (a < (b + carry_in))
-            ? s_f(cpu, FC) : c_f(cpu, FC);  // C Flag
+    s_f(cpu, FN);   // N (sub) Always set
+    (sub_16bit & 0xFF) == 0
+        ? s_f(cpu, FZ) : c_f(cpu, FZ);  // Z Flag
+    ((a & 0x0F) - (b & 0x0F) - carry_in) < 0
+        ? s_f(cpu, FH) : c_f(cpu, FH);  // H Flag
+    (a < (b + carry_in))
+        ? s_f(cpu, FC) : c_f(cpu, FC);  // C Flag
 }
 
 void prediction_inc8(uint8_t val, CPU *cpu) {
@@ -103,8 +111,45 @@ void prediction_dec8(uint8_t val, CPU *cpu) {
         s_f(cpu, FN);   // N (sub) Always Set
 }
 
+void prediction_and(uint8_t val, CPU *cpu) {
 
+}
 
+void prediction_xor(uint8_t val, CPU *cpu) {
+
+}
+
+void prediction_or(uint8_t val, CPU *cpu) {
+
+}
+
+void prediction_cp(uint8_t val, CPU *cpu) {
+
+}
+
+// 16bit.
+void prediction_inc16(uint8_t val, CPU *cpu) {
+    // No flags set.
+    // Probably delete this
+
+}
+
+void prediction_dec16(uint8_t val, CPU *cpu) {
+    // No flags set.
+    // Probably detete this
+
+}
+
+void prediction_add16(uint16_t a, uint16_t b, CPU *cpu) {
+    uint32_t add_result = (a + b);
+    
+    // Z FLAG UNCHANGED, Do not set, or clear.
+    c_f(cpu, FN);  // ALways cleared
+    ((a & 0x0FFF) + (b & 0x0FFF) > 0x0FFF) // The H Flag.
+        ? s_f(cpu, FH) : c_f(cpu, FH);
+    (add_result > 0xFFFF)
+        ? s_f(cpu, FC) : c_f(cpu, FC); // C Flag
+}
 
 
 
