948ce6fab6b47b2935a195cee7ee18c5fe3b18f3
EmmHarris343 <emilee.n.harrison@outlook.com>
2026-04-26 11:50:04 -0400

    fix: fake APU bug, needed 0x00 instead of 0xFF for registers.

--- x_emulation/src/core/apu/apu.c
+++ x_emulation/src/core/apu/apu.c
@@ -7,17 +7,31 @@ int apu_init(GB *gb) {
 }
 
 uint8_t apu_io_read(GB *gb, uint16_t addr){
+    if (addr >= 0xFF10 && addr <= 0xFF3F) {
+        return 0x00;
+    }
     return 0xFF;
 }
 uint8_t apu_wave_read(GB *gb, uint16_t addr) {
+    if (addr >= 0xFF10 && addr <= 0xFF3F) {
+        return 0x00;
+    }
     return 0xFF;
 }
 
 void apu_io_write(GB *gb, uint16_t addr, uint8_t write_val){
     (void)addr;
     (void)write_val;
+    if (addr >= 0xFF10 && addr <= 0xFF3F) {
+        return;
+    }
+    return;
 }
 void apu_wave_write(GB *gb, uint16_t addr, uint8_t write_val){
     (void)addr;
     (void)write_val;
+    if (addr >= 0xFF10 && addr <= 0xFF3F) {
+        return;
+    }
+    return;
 }
